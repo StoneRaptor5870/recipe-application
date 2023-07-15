66815277ccc83c675aa88716bf392260e967bf4c
StoneRaptor5870 <nischay.dtu@gmail.com>
2023-07-15 23:08:16 +0530

    recipe application

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '64796efc-a47a-4b09-99fe-1e88e93fb1e3';
+export const MODAL_CLOSE_SEC = 2.5;
